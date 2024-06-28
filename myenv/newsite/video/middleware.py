1fedd7f80fd2cf7596fddbe54496d025d9d19e43
theLoneCheetah <ils.mozhaev@gmail.com>
2024-06-28 10:14:14 +0300

    Database model and middleware created

---
+++ myenv/newsite/video/middleware.py
@@ -0,0 +1,14 @@
+from django.utils.deprecation import MiddlewareMixin
+from .models import RequestLog
+
+# перехват запроса для записи в базу данных
+class RequestMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        # сохранение инфомрации о запросе в базу данных
+        RequestLog.objects.create(
+            method=request.method,
+            path=request.path,
+            remote_addr=request.META.get('REMOTE_ADDR', ''),
+            query_params=request.GET.urlencode(),
+        )
+        return None
--- myenv/newsite/video/models.py
+++ myenv/newsite/video/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-class Person(models.Model):
-	firstname = models.CharField(max_length=255)
-	lastname = models.CharField(max_length=255)
-	phone = models.IntegerField(null=True)
-	joined_date = models.DateField(null=True)
+# создание модели для базы данных
+class RequestLog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)   # автоматическое добавление даты и времени
+    method = models.CharField(max_length=10)
+    path = models.CharField(max_length=255)
+    remote_addr = models.CharField(max_length=50, blank=True)
+    query_params = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.method} {self.path} at {self.timestamp}"
